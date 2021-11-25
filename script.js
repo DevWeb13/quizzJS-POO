4071a571d6457752f82de3e56f9a6c622db74406
DevWeb13 <giuliano.loic@sfr.fr>
2021-11-25 17:05:57 +0100

    feat(script.js): class Question + array questions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+class Question {
+	constructor(text, choices, answer) {
+		this.text = text;
+		this.choices = choices;
+		this.answer = answer;
+	}
+	isCorrectAnswer(choice) {
+		// Si la (bonne) réponse est égal au choix => true;
+		return this.answer === choice;
+	}
+}
 
+let questions = [
+	new Question(
+		"Quelle est le prénom de l'entraineur de Rocky?",
+		["Donald", "Mickey", "Dingo", "L'autre"],
+		"Mickey"
+	),
+	new Question(
+		"Dans quel film Rocky combat-il 'Barracuda' de 'L'agence tous risques'?",
+		["Le 1", "Le 2", "Le 3", "Le 123"],
+		"Le 3"
+	),
+	new Question(
+		"De quel nationalité est le boxeur qui combat Rocky dans le 4,",
+		["Russe", "Croate", "Roumain", "Clando"],
+		"Russe"
+	),
+	new Question(
+		"Quelle est le prénom de Rocky?",
+		["Apollo", "Rocca", "Rocco", "Vraiment conne comme question:->"],
+		"Vraiment conne comme question:->"
+	),
+];
